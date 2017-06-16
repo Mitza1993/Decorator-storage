e763bb1f8ad0042b7796707d434d0aa44c25f785
Stavarache, Diana <diana.stavarache@misys.com>
2017-06-16 16:52:00 +0300

    install angular material

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+    'ngMaterial',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version',
-  'directive.g+signin'
+  'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
